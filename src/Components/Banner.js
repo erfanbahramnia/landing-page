dc856061429b36be7da2ff46ac277bbc9bcb9c5d
Erfan Bahramnia <erfanbahramnia@gmail.com>
2022-05-05 12:42:30 +0430

    style of Banner component completed

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,18 +1,20 @@
 import React, {Component} from "react";
 
+// style of Banner
+import styles from "./Banner.module.css";
 
 class Banner extends Component {
     render() {
         return (
-            <div>
-                <img src="https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
-                <div>
+            <div className={styles.container}>
+                <img id={styles.bannerImage} src="https://images.unsplash.com/photo-1610792516775-01de03eae630?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                <div className={styles.content}>
                     <h1>Digi Phone</h1>
                     <p>get upgrade your <span>phone</span></p>
                 </div>
             </div>
         )
-    }
-}
+    };
+};
 
 export default Banner
