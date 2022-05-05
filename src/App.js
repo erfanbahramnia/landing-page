2d362ff5052f849349a2ea480aa242f04d57a88d
Erfan Bahramnia <erfanbahramnia@gmail.com>
2022-05-05 10:35:16 +0430

    Header component imported to App.js in displayed in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React , {Component} from "react";
 
+// Header component
+import Header from "./Header";
+
 class App extends Component {
 
     render() {
         return (
             <>
+                <Header />
             </>
         )
     }
