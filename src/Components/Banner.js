06a8b35990820c997b07b6ee22bc0ea8e9a7fae7
Erfan Bahramnia <erfanbahramnia@gmail.com>
2022-05-10 20:34:17 +0430

    the image of banner changed and address of image save in constant variable and get larger in height

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -5,9 +5,11 @@ import styles from "./Banner.module.css";
 
 class Banner extends Component {
     render() {
+        const imageAdd = "https://images.unsplash.com/photo-1585398998624-d32a984b7fad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+
         return (
             <div className={styles.container}>
-                <img id={styles.bannerImage} src="https://images.unsplash.com/photo-1610792516775-01de03eae630?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+                <img id={styles.bannerImage} src={imageAdd} />
                 <div className={styles.content}>
                     <h1>Digi Phone</h1>
                     <p>get upgrade your <span>phone</span></p>
