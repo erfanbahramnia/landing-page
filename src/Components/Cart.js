47c9dae8e767bb97793e8ebe55049422f8cbc4b3
Erfan Bahramnia <erfanbahramnia@gmail.com>
2022-05-10 20:27:11 +0430

    the style sheet of cart component completed

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,16 +1,18 @@
 import React, {Component} from "react";
 
+// style
+import styles from "./Cart.module.css";
 
 class Cart extends Component {
     render() {
         return (
-            <div>
-                <img src={this.props.imageAdd} />
-                <div>
+            <div className={styles.container}>
+                <img src={this.props.imageAdd} alt="phone" className={styles.image} />
+                <div className={styles.phoneData}>
                     <p>{this.props.phoneName}</p>
                 </div>
-                <div>
-                    <p>{this.props.phonePrice}</p>
+                <div className={styles.buyPhone}>
+                    <p className={styles.phonePrice}>{this.props.phonePrice}</p>
                 </div>
             </div>
         )
