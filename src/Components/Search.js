32a6bf6306134de45a6cf02ad2e8bdcee46db47a
Erfan Bahramnia <erfanbahramnia@gmail.com>
2022-05-10 21:02:32 +0430

    changed value with placeholder in input and imported style in component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,12 +1,14 @@
 import React, {Component} from "react";
 
+// style
+import styles from "./Search.module.css";
 
 class Search extends Component {
     render() {
         return (
-            <div>
+            <div className={styles.container}>
                 <h1>Search What You Want</h1>
-                <input type="text" value="search..." />
+                <input type="text" placeholder="search..." />
             </div>
         )
     };
